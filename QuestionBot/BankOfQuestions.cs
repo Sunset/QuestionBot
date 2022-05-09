9b277af172d9c68f49ce4ca5b6d9a10932d43db6
ahsia1 <allenhsia1@gmail.com>
2022-05-08 17:53:26 -0700

    initial

---
+++ QuestionBot/BankOfQuestions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestionBot
+{
+    public class BankOfQuestions
+    {
+        public List<string> InitializeList()
+        {
+            List<string> QuestionList = new List<string>();
+
+            foreach (string line in System.IO.File.ReadLines(@"<Put your local drive file here>"))
+            {
+                QuestionList.Add(line);
+            }
+
+            return QuestionList;
+        }
+
+        public List<string> AddQuestion(List<string> QuestionList, string Question)
+        {
+            QuestionList.Add((QuestionList.Count + 1 + " ") + Question);
+            return QuestionList;
+        }
+
+        public List<string> DeleteQuestion(List<string> QuestionList, int QuestionCounter)
+        {
+            QuestionList.RemoveAt(QuestionCounter-1);
+            return QuestionList;
+        }
+
+        public string PickQuestion(List<string> QuestionList)
+        {
+            Random rnd = new Random();
+            int QuestionCounter = rnd.Next(0, QuestionList.Count()-1);
+            return QuestionList[QuestionCounter];
+        }
+
+        public string DisplayQuestionList(List<string> QuestionList)
+        {
+            string QuestionListText = string.Empty;
+            int counter = 1;
+            foreach(string Question in QuestionList)
+            {
+                QuestionListText += counter + " " + Question + '\n';
+                counter++;
+            }
+            return QuestionListText;
+        }
+    }
+}
---
+++ QuestionBot/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QuestionBot
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Initialize question object
+            BankOfQuestions boq = new BankOfQuestions();
+            List<string> QuestionList = boq.InitializeList();
+            var Display = boq.DisplayQuestionList(QuestionList);
+
+            //Add Question
+            Console.WriteLine("What question would you like to add?");
+            string QuestionToAdd = Console.ReadLine();
+            QuestionList = boq.AddQuestion(QuestionList, QuestionToAdd);
+
+            //Display question list
+            Console.WriteLine(Display);
+
+            //pick a random Question
+            string PickedQuestion = boq.PickQuestion(QuestionList);
+            Console.WriteLine(PickedQuestion);
+
+            //Remove questions
+            Console.WriteLine(Display);
+            Console.WriteLine("Which question do you want to remove?");
+            string strQuestionCounter = Console.ReadLine();
+            int QuestionCounter = Convert.ToInt32(strQuestionCounter);
+            QuestionList = boq.DeleteQuestion(QuestionList, QuestionCounter);
+            var NewDisplay = boq.DisplayQuestionList(QuestionList);
+            Console.WriteLine(NewDisplay);
+        }
+    }
+}
